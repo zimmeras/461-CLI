854e164af0d03d0e475695e8dbe36f3c6ee70f60
zimmeras <zimmeras@purdue.edu>
2023-01-25 20:04:51 -0500

    rust set up

---
+++ cli/target/debug/deps/cli-4f4f87f0dd8a098d.d
@@ -0,0 +1,5 @@
+/Users/andrewzimmerman/Documents/Purdue/Junior-Spring 2023/ECE 461/CLI-Project/461-CLI/cli/target/debug/deps/cli-4f4f87f0dd8a098d: src/main.rs
+
+/Users/andrewzimmerman/Documents/Purdue/Junior-Spring 2023/ECE 461/CLI-Project/461-CLI/cli/target/debug/deps/cli-4f4f87f0dd8a098d.d: src/main.rs
+
+src/main.rs:
