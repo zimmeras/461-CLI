ba25b262036f0a63359d43183a17a85ddb4f36ab
zimmeras <zimmeras@purdue.edu>
2023-01-26 12:49:03 -0500

    initial set up v3

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+pub mod rate_repos {
+    pub mod metrics {
+        pub mod bus_factor;
+        pub mod correctness;
+        pub mod license;
+        pub mod ramp_up;
+        pub mod responsive_maintainer;
+    }
+
+    pub fn parse_url_file(_url_file_path: &str) {
+        use metrics::bus_factor;
+        let bf_score = bus_factor::get_bf();
+        println!("Bus factor score {bf_score}");
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use std::env;
+use purdue461_cli::rate_repos;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    
+    let input = &args[1];
+    match &input[..] {
+        "install" => install(),
+        "build" => build(),
+        "tes" => test(),
+        url_file_path => rate_repos::parse_url_file(url_file_path),
+    };
+}
+
+fn install() {
+    println!("Installing dependencies");
+}
+
+fn build() {}
+fn test() {}
