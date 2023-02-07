b718ba780081a969204fbe3592d76b027d0ffedf
zimmeras <zimmeras@purdue.edu>
2023-02-07 01:24:20 -0500

    added bash script, fixed printing, validated github links

--- Readme.md
+++ Readme.md
@@ -1,11 +1,3 @@
 Purdue 461 CLI project. Spring 2023
 
-to create the executable we will eventually need to submit do:
-
-cargo build -r
-
-and then the executable will be called "purdue461_cli" and will be in /target/release
-we will just have to rename that executable to "run"
-
-
-will need to do log file stuff too
+run the this project using ./run ex_url_file.txt
--- src/lib.rs
+++ src/lib.rs
@@ -1,55 +1,61 @@
 pub mod rate_repos {
+    use serde::{Serialize, Deserialize};
     pub mod metrics {
+        use super::*;
+
         pub mod bus_factor;
         pub mod correctness;
         pub mod license;
         pub mod ramp_up;
         pub mod responsive_maintainer;
 
-        #[derive(Debug)]
+        #[derive(Serialize, Deserialize, Debug)]
+        #[serde(rename_all = "UPPERCASE")]
         pub struct MetricScores {
             pub net_score: f32,
-            pub bus_factor_score: f32,
+            pub ramp_up_score: i32,
             pub correctness_score: f32,
-            pub license_score: f32,
-            pub ramp_up_score: f32,
+            pub bus_factor_score: f32,
             pub responsive_maintainer_score: f32,
+            pub license_score: f32,
         }
 
-        const BUS_FACTOR_WEIGHT: f32 = 0.15;
-        const CORRECTNESS_WEIGHT: f32 = 0.15;
-        const LICENSE_WEIGHT: f32 = 0.15;
-        const RAMP_UP_WEIGHT: f32 = 0.15;
+        const BUS_FACTOR_WEIGHT: f32 = 0.2;
+        const CORRECTNESS_WEIGHT: f32 = 0.2;
+        const LICENSE_WEIGHT: f32 = 0.2;
+        const RAMP_UP_WEIGHT: i32 = 0;
         const RESPONSIVE_MAINTAINER_WEIGHT: f32 = 0.4;
 
         pub fn get_metrics(_url: &str) -> MetricScores {
             // each of these 0.5's will be a call each metric function in their file
             let mut scores = MetricScores {
                 net_score: 0.0,
-                bus_factor_score: 0.5,
+                ramp_up_score: -1,
                 correctness_score: 0.5,
-                license_score: 0.5,
-                ramp_up_score: 0.5,
+                bus_factor_score: 0.5,
                 responsive_maintainer_score: 0.5,
+                license_score: 0.5,
             };
 
             scores.net_score =  scores.bus_factor_score * BUS_FACTOR_WEIGHT +
                                 scores.correctness_score * CORRECTNESS_WEIGHT +
                                 scores.license_score * LICENSE_WEIGHT +
-                                scores.ramp_up_score * RAMP_UP_WEIGHT +
-                                scores.responsive_maintainer_score * RESPONSIVE_MAINTAINER_WEIGHT;
+                                scores.responsive_maintainer_score * RESPONSIVE_MAINTAINER_WEIGHT +
+                                (scores.ramp_up_score * RAMP_UP_WEIGHT) as f32;
 
             return scores;
         }
     }
 
-    #[derive(Debug)]
+    #[derive(Serialize, Deserialize, Debug)]
+    #[serde(rename_all = "UPPERCASE")]
     pub struct UrlSpecs {
         url: String,
+        #[serde(flatten)]
         metric_scores: metrics::MetricScores,
     }
 
-    fn get_github_url_for_npm(npm_url: &str) -> Result<String, ureq::Error>{
+    fn get_github_url_for_npm(npm_url: &str) -> Result<String, ureq::Error> {
         let url = format!("https://registry.npmjs.org/{}", &npm_url[30..]);
         let json: serde_json::Value = ureq::get(&url).call()?.into_json()?;
         let repo_info = &json["repository"];
@@ -70,6 +76,16 @@ pub mod rate_repos {
         }
     }
 
+    fn validate_github_url(url: &str) -> Result<bool, ureq::Error> {
+        let repo_full_name = &url[19..];
+        let http_url = format!("https://api.github.com/repos/{}", &repo_full_name);
+        let response = ureq::get(&http_url).call();
+        match response {
+            Ok(_r) => return Ok(true),
+            Err(_e) => return Ok(false),
+        };
+    }
+
     pub fn rate_repos(url_file_path: &str) {
         use std::fs;
         let file_contents = fs::read_to_string(url_file_path).expect("Should have been able to read the file");
@@ -81,38 +97,63 @@ pub mod rate_repos {
             if &url[0..22] == "https://www.npmjs.com/" {
                 let github_url = get_github_url_for_npm(&url).unwrap();
                 if &github_url[0..19] == "https://github.com/" {
-                    let url_spec = UrlSpecs {
-                        url: url.to_string(),
-                        metric_scores: metrics::get_metrics(&github_url),
-                    };
-                    url_specs.push(url_spec);
+                    if validate_github_url(&github_url).unwrap() {
+                        let url_spec = UrlSpecs {
+                            url: url.to_string(),
+                            metric_scores: metrics::get_metrics(&github_url),
+                        };
+                        url_specs.push(url_spec);
+                    }
+                    else {
+                        let url_spec = UrlSpecs {
+                            url: url.to_string(),
+                            metric_scores: metrics::MetricScores {
+                                net_score: 0.0,
+                                ramp_up_score: -1,
+                                correctness_score: 0.0,
+                                bus_factor_score: 0.0,
+                                responsive_maintainer_score: 0.0,
+                                license_score: 0.0,
+                            },
+                        };
+                        url_specs.push(url_spec);
+                    }
                 }
             }
             else if &url[0..19] == "https://github.com/" {
-                let url_spec = UrlSpecs {
-                    url: url.to_string(),
-                    metric_scores: metrics::get_metrics(&url),
-                };
-                url_specs.push(url_spec);
+                if validate_github_url(&url).unwrap() {
+                    let url_spec = UrlSpecs {
+                        url: url.to_string(),
+                        metric_scores: metrics::get_metrics(&url),
+                    };
+                    url_specs.push(url_spec);
+                }
+                else {
+                    let url_spec = UrlSpecs {
+                        url: url.to_string(),
+                        metric_scores: metrics::MetricScores {
+                            net_score: 0.0,
+                            ramp_up_score: -1,
+                            correctness_score: 0.0,
+                            bus_factor_score: 0.0,
+                            responsive_maintainer_score: 0.0,
+                            license_score: 0.0,
+                        },
+                    };
+                    url_specs.push(url_spec);
+                }
             }
         }
 
         // sort the repos in decreasing order
         url_specs.sort_by(|a, b| b.metric_scores.net_score.partial_cmp(&a.metric_scores.net_score).unwrap());
         
-        print_metrics(&url_specs);
+        print_url_specs(&url_specs);
     }
 
-    pub fn print_metrics(url_specs: &Vec<UrlSpecs>) {
-        println!("URL NET_SCORE RAMP_UP_SCORE CORRECTNESS_SCORE BUS_FACTOR_SCORE RESPONSIVE_MAINTAINER_SCORE LICENSE_SCORE");
-        for repo in url_specs {
-            print!("{}", repo.url);
-            print!(" {}", repo.metric_scores.net_score);
-            print!(" {}", repo.metric_scores.ramp_up_score);
-            print!(" {}", repo.metric_scores.correctness_score);
-            print!(" {}", repo.metric_scores.bus_factor_score);
-            print!(" {}", repo.metric_scores.responsive_maintainer_score);
-            println!(" {}", repo.metric_scores.license_score);
+    pub fn print_url_specs(url_specs: &Vec<UrlSpecs>) {
+        for repo_info in url_specs {
+            println!("{}", serde_json::to_string(&repo_info).unwrap());
         }
     }
 }
